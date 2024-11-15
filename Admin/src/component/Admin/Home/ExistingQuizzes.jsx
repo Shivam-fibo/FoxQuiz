9c5cc0f2b55c594dc1c0acd94df7e5bedc1d1c21
Shivam <sg641818@gmail.com>
2024-11-15 18:35:59 +0530

    added delete quiz logic in frontend

--- Admin/src/component/Admin/Home/ExistingQuizzes.jsx
+++ Admin/src/component/Admin/Home/ExistingQuizzes.jsx
@@ -12,6 +12,17 @@ const ExistingQuizzes = () => {
     const response = await axios.get("http://localhost:3000/quiz/allQuiz");
     setQuizzes(response.data.data);
   };
+
+  const  deleteQuiz =  async(quizId) => {
+   try {
+    await axios.delete(`http://localhost:3000/quiz/${quizId}`)
+    toast.success("Quiz Deleted Successfully")
+      fetchQuizzes();
+   } catch (error) {
+      toast.error("Something went wrong")
+   }  
+
+  }
   
   return (
     <div>
@@ -19,14 +30,24 @@ const ExistingQuizzes = () => {
   <h3 className="text-lg font-semibold mt-8">Existing Quizzes</h3>
   <ul>
     {quizzes.length> 0 ? quizzes.map((quiz) => (
-      <li key={quiz._id} className="border border-gray-300 p-4 rounded-md my-2 flex justify-between items-center bg-white shadow-sm">
+      <li key={quiz._id} className="border border-gray-300 p-4 rounded-md my-2 mx-2 flex justify-between items-center bg-white shadow-sm">
+        <div>
+
+     
         <span>{quiz.title}</span>
+        </div>
+
+        <div className=''>
+
+   
+        <button onClick={() => getQuizAllInfo(quiz._id)}  className='text-blue-500 hover:underline transition duration-250'>Get info </button>
         <button
           onClick={() => deleteQuiz(quiz._id)}
-          className="text-red-500 hover:underline transition duration-200"
+          className=" ml-4 text-red-500 hover:underline transition duration-200"
         >
           Delete
         </button>
+        </div>
       </li>
     )) : <p>No quiz added</p>}
   </ul> 
